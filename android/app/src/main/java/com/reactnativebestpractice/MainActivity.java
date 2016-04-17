9696cf77d0a45260ddcb480d4b683667987d97ea
wjljack <wjltsss@163.com>
2016-04-17 22:34:34 +0800

    add splash screen to android

--- android/app/src/main/java/com/reactnativebestpractice/MainActivity.java
+++ android/app/src/main/java/com/reactnativebestpractice/MainActivity.java
@@ -1,8 +1,9 @@
 package com.ReactNativeBestPractice;
-
 import com.facebook.react.ReactActivity;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
+import com.remobile.splashscreen.*;
+
 
 import java.util.Arrays;
 import java.util.List;
@@ -34,7 +35,8 @@ public class MainActivity extends ReactActivity {
     @Override
     protected List<ReactPackage> getPackages() {
         return Arrays.<ReactPackage>asList(
-            new MainReactPackage()
+                new MainReactPackage(),
+                new RCTSplashScreenPackage(this)
         );
     }
 }
