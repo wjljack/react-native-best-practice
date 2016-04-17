c0b342edff142a2452e489123df2f9df3a6e6259
wjljack <wjltsss@163.com>
2016-04-17 21:06:45 +0800

    rename project to ReactNativeBestPractice

--- android/app/src/main/java/com/reactnativeeasyboilerplate/MainActivity.java
+++ android/app/src/main/java/com/reactnativeeasyboilerplate/MainActivity.java
@@ -1,4 +1,4 @@
-package com.reactnativeeasyboilerplate;
+package com.ReactNativeBestPractice;
 
 import com.facebook.react.ReactActivity;
 import com.facebook.react.ReactPackage;
@@ -15,7 +15,7 @@ public class MainActivity extends ReactActivity {
      */
     @Override
     protected String getMainComponentName() {
-        return "ReactNativeEasyBoilerplate";
+        return "ReactNativeBestPractice";
     }
 
     /**
