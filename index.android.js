5bdbf60afcd5ba6081c009181255d7569568fd58
Erick Lau <181621732@qq.com>
2016-04-17 12:58:00 +0800

    Added files via upload

---
+++ index.android.js
@@ -0,0 +1,12 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ */
+'use strict';
+import React, {
+    AppRegistry
+} from 'react-native';
+import Index from './src/main'
+
+
+AppRegistry.registerComponent('ReactNativeEasyBoilerplate',() => Index);
